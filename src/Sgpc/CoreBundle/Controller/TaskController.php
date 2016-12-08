81b5d122a455e7e97e95dc8d5622a5d57d62d846
Samuel Torres <samuel.torres@e-campus.uab.es>
2016-12-08 18:48:00 +0100

    archivado de tarea por miembro de proyecto

--- src/Sgpc/CoreBundle/Controller/TaskController.php
+++ src/Sgpc/CoreBundle/Controller/TaskController.php
@@ -4,25 +4,23 @@ namespace Sgpc\CoreBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response; 
+use Symfony\Component\HttpFoundation\Response;
 use Sgpc\CoreBundle\Entity\Task;
 use Sgpc\CoreBundle\Form\TaskType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
-class TaskController extends Controller
-{
+class TaskController extends Controller {
 
     /**
      * Crea una nueva entidad Task
      */
-    public function createAction(Request $request, $id)
-    {
+    public function createAction(Request $request, $id) {
         $entity = new Task();
-        $parent = $this->getDoctrine()->getRepository('SgpcCoreBundle:Listing')->findOneById($id);  
+        $parent = $this->getDoctrine()->getRepository('SgpcCoreBundle:Listing')->findOneById($id);
         $project = $parent->getProject();
         $form = $this->createForm(new TaskType(), $entity);
         $form->handleRequest($request);
-        
+
         if ($form->isValid()) {
             $entity->setListing($parent);
             $entity->setProject($project);
@@ -31,79 +29,80 @@ class TaskController extends Controller
             $em->flush();
             return $this->redirect($this->generateUrl('sgpc_task_view', array('id' => $entity->getId())));
         }
-        
+
         return $this->render('SgpcCoreBundle:Task:new.html.twig', array(
-            'entity' => $entity,
-            'form'   => $form->createView(),
+                    'entity' => $entity,
+                    'form' => $form->createView(),
         ));
     }
-    
-        
+
     /**
      * Display del form para crear una nueva entidad Task
      */
-    public function addAction($id)
-    {
+    public function addAction($id) {
         $entity = new Task();
-        
+
         $form = $this->createForm(new TaskType(), $entity, array(
             'action' => $this->generateUrl('sgpc_task_create', array('id' => $id)),
-        ));  
-        
+        ));
+
         return $this->render('SgpcCoreBundle:Task:add.html.twig', array(
-            'entity' => $entity,
-            'create_form'   => $form->createView(),
+                    'entity' => $entity,
+                    'create_form' => $form->createView(),
         ));
     }
+
     /**
      * Muestra la entidad Task
      */
-    public function viewAction($id)
-    {
+    public function viewAction($id) {
         $em = $this->getDoctrine()->getManager();
-        $task = $em->getRepository('SgpcCoreBundle:Task')->find($id);      
-        
+        $task = $em->getRepository('SgpcCoreBundle:Task')->find($id);
+
         if (!$task) {
             throw $this->createNotFoundException('Unable to find Task entity.');
         }
-        
+
         $comments = $em->getRepository('SgpcCoreBundle:Comment')
-                   ->getCommentsForTask($task->getId());
-        
+                ->getCommentsForTask($task->getId());
+
+        $storeForm = $this->createStoreForm($id);
+
+
         return $this->render('SgpcCoreBundle:Task:view.html.twig', array(
-            'task'      => $task,
-            'comments' => $comments
+                    'task' => $task,
+                    'comments' => $comments,
+                    'store_form' => $storeForm->createView()
         ));
     }
-    
-    
-     /*
+
+    /*
      * Crea el formulario para añadir un miembro a la tarea, comprobando que solo
-      * se puedan seleccionar los miembros del proyecto del cual depende la tarea
+     * se puedan seleccionar los miembros del proyecto del cual depende la tarea
      */
-    private function createAddMemberForm($id)
-    {        
+
+    private function createAddMemberForm($id) {
         return $this->createFormBuilder()
-            ->setAction($this->generateUrl('sgpc_task_addmember', array('id' => $id)))
-            ->setMethod('POST')
-            ->add('user', 'choice', array(
-                'choices' => $this->arrayToChoices($id),
-            ))
-            ->add('submit', 'submit', array('label' => 'Añadir usuario'))
-            ->getForm();
+                        ->setAction($this->generateUrl('sgpc_task_addmember', array('id' => $id)))
+                        ->setMethod('POST')
+                        ->add('user', 'choice', array(
+                            'choices' => $this->arrayToChoices($id),
+                        ))
+                        ->add('submit', 'submit', array('label' => 'Añadir usuario'))
+                        ->getForm();
     }
-    
+
     /**
      * Función para pasar los usuarios miembros de un proyecto como array 
      * al formulario de createAddMemberForm($id)
      */
-    protected function arrayToChoices($id){
-        
+    protected function arrayToChoices($id) {
+
         $em = $this->getDoctrine()->getManager();
         $task = $em->getRepository('SgpcCoreBundle:Task')->find($id);
         $list = $task->getListing();
-        $project = $list->getProject()->getId();      
-        
+        $project = $list->getProject()->getId();
+
         /*
          * ToDo: poner en repository. Seleccionamos los usuarios que son miembros
          * del proyecto y no son miembros de esta tarea para pasarlos al form como choices
@@ -114,9 +113,9 @@ class TaskController extends Controller
             'idTask' => $id,
             'idProject' => $project,
         ));
-        $projectMembers = $query->getResult();    
+        $projectMembers = $query->getResult();
 
-        foreach($projectMembers as $projectMember){         
+        foreach ($projectMembers as $projectMember) {
             $choices[$projectMember->getId()] = $projectMember->getUsername();
         }
         if (empty($choices)) {
@@ -124,39 +123,68 @@ class TaskController extends Controller
         }
         return $choices;
     }
-    
-    
+
     /**
-     *Añade un miembro a la tarea
+     * Añade un miembro a la tarea
      */
-    public function addMemberAction(Request $request, $id)
-    {
+    public function addMemberAction(Request $request, $id) {
         $em = $this->getDoctrine()->getManager();
         $form = $this->createAddMemberForm($id);
         $form->handleRequest($request);
-        
+
         $task = $em->getRepository('SgpcCoreBundle:Task')->findOneById($id);
-        
+
         if ($form->isValid()) {
-            
+
             $formuserId = $form->get('user')->getData();
-       
+
             $user = $em->getRepository('SgpcCoreBundle:User')->findOneBy(array('id' => $formuserId));
-            if(!$user){
+            if (!$user) {
                 throw $this->createNotFoundException('No se ha encontrado el usuario.');
-            } 
+            }
             $task->addUser($user);
             $em->persist($task);
             $em->persist($user);
             $em->flush();
 
             return $this->redirect($this->generateUrl('sgpc_task_view', array('id' => $task->getId())));
-        }       
-        
-         return $this->render('SgpcCoreBundle:Task:addmember.html.twig', array(
-            'task' => $task,
-            'addmember_form'   => $form->createView(),
+        }
+
+        return $this->render('SgpcCoreBundle:Task:addmember.html.twig', array(
+                    'task' => $task,
+                    'addmember_form' => $form->createView(),
         ));
     }
-    
+
+    /* crea el formulario para archivar una tarea desde el desplegable */
+
+    private function createStoreForm($id) {
+        return $this->createFormBuilder()
+                        ->setAction($this->generateUrl('sgpc_task_store', array('id' => $id)))
+                        ->setMethod('POST')
+                        ->add('submit', 'submit', array('label' => 'Archivar tarea'))
+                        ->getForm();
+    }
+
+    /* Accion que se encarga de archivar una tarea */
+
+    public function storeAction(Request $request, $id) {
+        $form = $this->createStoreForm($id);
+        $form->handleRequest($request);
+
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $task = $em->getRepository('SgpcCoreBundle:Task')->find($id);
+            if (!$task) {
+                throw $this->createNotFoundException('No se ha encontrado la entidad tarea.');
+            }
+
+            $task->setIsActive(false);
+            $em->persist($task);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('sgpc_task_view', array('id' => $task->getId())));
+        }
+    }
+
 }
